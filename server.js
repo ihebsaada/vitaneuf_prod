e51907144a41b61b17ae4779794258fb4ba3e904
ihebsaada <83327619+ihebsaada@users.noreply.github.com>
2025-10-16 19:55:53 +0100

    Remove MONGO_URI console log
    Removed console log for MONGO_URI.

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 dotenv.config();
-console.log('📦 Loaded MONGO_URI:', process.env.MONGO_URI);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
